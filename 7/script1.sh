1e3c5cc464bada78ee7220ccbe9f35e081bf0c8d
wequalwo <s.k.ivanov314@gmail.com>
2022-03-27 21:40:18 +0300

    7 last fix before 2d a-t

--- 7/script1.sh
+++ 7/script1.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 g++ -pthread -o p1 OS_lab7_1.cpp -lrt
+xattr -l p1
+sudo setcap cap_sys_resource=eip p1
+xattr -l p1
 ./p1
--- 7/script2.sh
+++ 7/script2.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 g++ -pthread -o p2 OS_lab7_2.cpp -lrt
+xattr -l p2
+sudo setcap cap_sys_resource=eip p2
+xattr -l p2
 ./p2
