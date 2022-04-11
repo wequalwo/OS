1cc9832704b883f5d733fc6c2eab9257c01d0d16
wequalwo <s.k.ivanov314@gmail.com>
2022-04-11 19:26:56 +0300

    v1.2

--- 10/OS_lab10.cpp
+++ 10/OS_lab10.cpp
@@ -2,9 +2,11 @@
 #include <ucontext.h>
 #include <unistd.h>
 
-ucontext_t uctx_func1;
-ucontext_t uctx_func2;
-ucontext_t uctx_main;
+#define SIZE 16384
+
+ucontext_t *uctx_func1;
+ucontext_t *uctx_func2;
+ucontext_t *uctx_main;
 
 void func1(void)
 {
@@ -13,9 +15,8 @@ void func1(void)
         std::cout << "1\n"
                   << std::flush;
         sleep(1);
-        // передать управление программе 2
         std::cout << "Swap context: 1->2\n";
-        swapcontext(&uctx_func1, &uctx_func2);
+        swapcontext(uctx_func1, uctx_func2);
     }
 }
 
@@ -24,18 +25,18 @@ void func2(void)
     int count = 0;
     while (1)
     {
-        std::cout << "2\n" << std::flush;
+        std::cout << "2\n"
+                  << std::flush;
         sleep(1);
-        if (count > 3)
+        if (count >= 2)
         {
-            // передать управление сопрограмме М;
             std::cout << "Swap context: 2->M\n";
-            swapcontext(&uctx_func2, &uctx_main);
+            swapcontext(uctx_func2, uctx_main);
         }
         else
         {
             std::cout << "Swap context: 2->1\n";
-            swapcontext(&uctx_func2, &uctx_func1);
+            swapcontext(uctx_func2, uctx_func1);
         }
         count++;
     }
@@ -43,26 +44,35 @@ void func2(void)
 
 int main()
 {
-    char *func1_stack = new char[16384];
-    char *func2_stack = new char[16384];
+    char *func1_stack = new char[SIZE];
+    char *func2_stack = new char[SIZE];
 
-    getcontext(&uctx_func1);
-    uctx_func1.uc_stack.ss_sp = func1_stack;
-    uctx_func1.uc_stack.ss_size = sizeof(func1_stack);
-    makecontext(&uctx_func1, func1, 0);
+    uctx_func1 = new ucontext_t;
+    uctx_func2 = new ucontext_t;
+    uctx_main = new ucontext_t;
 
-    getcontext(&uctx_func2);
-    uctx_func2.uc_stack.ss_sp = func2_stack;
-    uctx_func2.uc_stack.ss_size = sizeof(func2_stack);
-    makecontext(&uctx_func2, func2, 0);
+    getcontext(uctx_func1);
+    (*uctx_func1).uc_stack.ss_sp = (void *)func1_stack;
+    (*uctx_func1).uc_stack.ss_size = SIZE;
+    makecontext(uctx_func1, func1, 0);
 
-    getcontext(&uctx_main);
+    getcontext(uctx_func2);
+    (*uctx_func2).uc_stack.ss_sp = (void *)func2_stack;
+    (*uctx_func2).uc_stack.ss_size = SIZE;
+    makecontext(uctx_func2, func2, 0);
+
+    getcontext(uctx_main);
 
     std::cout << "Swap context: M->1\n";
-    swapcontext(&uctx_main, &uctx_func1);
+    swapcontext(uctx_main, uctx_func1);
+
+    delete uctx_func1;
+    delete uctx_func2;
+    delete uctx_main;
 
     delete[] func1_stack;
     delete[] func2_stack;
+
     std::cout << "The prog has finished\n";
     return 0;
 }
